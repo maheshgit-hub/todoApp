0cb0db25b16e58be812a9244dbee0bb2cdcb90c3
Mahesh <mahesh001.mg@gmail.com>
2025-05-02 21:11:40 +0530

    Real date time

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdCheck, MdDelete } from "react-icons/md";
 import "./index.css";
 const Todo = () => {
   const [input, setInput] = useState("");
   const [task, setTask] = useState([]);
+  const [dateTime, setDateTime] = useState("");
   const handleInputChange = (value) => {
     setInput(value);
   };
@@ -17,16 +18,21 @@ const Todo = () => {
     setTask((prev) => [...prev, input]);
     setInput("");
   };
-  const now = new Date();
-  const formattedDate = now.toLocaleDateString();
-  const formattedTime = now.toLocaleTimeString();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const formattedDate = now.toLocaleDateString();
+      const formattedTime = now.toLocaleTimeString();
+      setDateTime(`${formattedDate} - ${formattedTime}`);
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
-        <h2 className="date-time">
-          Date{formattedDate}-Time{formattedTime}
-        </h2>
+        <h2 className="date-time">{dateTime}</h2>
       </header>
       <section className="form">
         <form onSubmit={handleFormSubmit}>
